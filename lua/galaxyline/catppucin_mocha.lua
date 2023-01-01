8cf4e2d35134e7b8ac43ad9bbe08895b0dba4680
Mike Rivnak <rivnakm1@gmail.com>
2023-01-01 12:53:28 -0500

    use catppucin theme

---
+++ lua/galaxyline/catppucin_mocha.lua
@@ -0,0 +1,12 @@
+return {
+  bg = "#1e1e2e",
+  fg = "#cdd6f4",
+  fg_alt = "#6c7086",
+  yellow = "#f9e2af",
+  cyan = "#94e2d5",
+  green = "#a6e3a1",
+  orange = "#fab387",
+  magenta = "#cba6f7",
+  blue = "#89b4fa",
+  red = "#f38ba8",
+}
