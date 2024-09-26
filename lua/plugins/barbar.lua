7e99f85118c4e6a948b6ec11c43b3d4592284d3f
Mike Rivnak <rivnakm1@gmail.com>
2024-09-25 20:53:21 -0400

    Improve barbar keybinds

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -9,14 +9,27 @@ return {
 	end,
 	event = "BufEnter",
 	config = function()
-		vim.keymap.set("n", "<leader>bn", "<Cmd>BufferNext<CR>", { noremap = true, silent = true, desc = "Next tab" })
+		vim.keymap.set("n", "<leader>bj", "<Cmd>BufferNext<CR>", { noremap = true, silent = true, desc = "Next tab" })
 		vim.keymap.set(
 			"n",
-			"<leader>bp",
+			"<leader>bf",
 			"<Cmd>BufferPrevious<CR>",
 			{ noremap = true, silent = true, desc = "Previous tab" }
 		)
+		vim.keymap.set(
+			"n",
+			"<leader>bmj",
+			"<Cmd>BufferMoveNext<CR>",
+			{ noremap = true, silent = true, desc = "Move tab right" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>bmf",
+			"<Cmd>BufferMovePrevious<CR>",
+			{ noremap = true, silent = true, desc = "Move tab left" }
+		)
 		vim.keymap.set("n", "<leader>bx", "<Cmd>BufferPin<CR>", { noremap = true, silent = true, desc = "Pin tab" })
+		vim.keymap.set("n", "<leader>bk", "<Cmd>BufferPick<CR>", { noremap = true, silent = true, desc = "Pick tab" })
 		vim.keymap.set("n", "<leader>bc", "<Cmd>BufferClose<CR>", { noremap = true, silent = true, desc = "Close tab" })
 		vim.keymap.set(
 			"n",
@@ -26,6 +39,7 @@ return {
 		)
 
 		require("barbar").setup({
+			animation = false,
 			sidebar_filetypes = {
 				NvimTree = true,
 			},
