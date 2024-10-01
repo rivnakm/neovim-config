410b25248fa9338a07398124725343ac6568ea7a
Mike Rivnak <7389355+rivnakm@users.noreply.github.com>
2024-10-01 10:56:36 -0400

    ignore which-key warnings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,7 +2,9 @@ return {
 	"folke/which-key.nvim",
 	event = "VimEnter",
 	config = function()
-		require("which-key").setup()
+		require("which-key").setup({
+			notify = false,
+		})
 
 		require("which-key").register({
 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
