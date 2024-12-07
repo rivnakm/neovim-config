919ff7b9bbe52b246cc153a5b1af37fe2be177a1
Mike Rivnak <rivnakm1@gmail.com>
2024-12-06 21:28:12 -0500

    fix barbar diag icon spacing

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -45,8 +45,8 @@ return {
 			},
 			icons = {
 				diagnostics = {
-					[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "" },
-					[vim.diagnostic.severity.WARN] = { enabled = true, icon = "" },
+					[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+					[vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
 					[vim.diagnostic.severity.INFO] = { enabled = false },
 					[vim.diagnostic.severity.HINT] = { enabled = false },
 				},
