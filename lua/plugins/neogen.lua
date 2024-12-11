786210746ce29159d72026aff35f33fd18007d9e
Mike Rivnak <rivnakm1@gmail.com>
2024-12-10 19:31:23 -0500

    Add neogen keybinds

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,5 +1,37 @@
 -- Documentation annotation provider
 return {
 	"danymat/neogen",
-	config = true,
+	config = function()
+		require("neogen").setup({})
+		vim.keymap.set(
+			"n",
+			"<leader>nda",
+			"<Cmd>Neogen<CR>",
+			{ noremap = true, silent = true, desc = "[N]eogen [D]ocument [A]ny" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ndf",
+			"<Cmd>Neogen func<CR>",
+			{ noremap = true, silent = true, desc = "[N]eogen [D]ocument [F]unction" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ndc",
+			"<Cmd>Neogen class<CR>",
+			{ noremap = true, silent = true, desc = "[N]eogen [D]ocument [C]lass" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ndt",
+			"<Cmd>Neogen type<CR>",
+			{ noremap = true, silent = true, desc = "[N]eogen [D]ocument [T]ype" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ndi",
+			"<Cmd>Neogen file<CR>",
+			{ noremap = true, silent = true, desc = "[N]eogen [D]ocument F[i]le" }
+		)
+	end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,15 +8,16 @@ return {
 		})
 
 		require("which-key").register({
+			["<leader>b"] = { name = "[B]arbar", _ = "which_key_ignore" },
 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
 			["<leader>d"] = { name = "[D]ocument/[D]iffview", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "nvim-[T]ree/[T]erminal", _ = "which_key_ignore" },
-			["<leader>b"] = { name = "[B]arbar", _ = "which_key_ignore" },
+			["<leader>n"] = { name = "[N]eogit/[N]eogen", _ = "which_key_ignore" },
 			["<leader>o"] = { name = "P[o]ssession", _ = "which_key_ignore" },
 			["<leader>p"] = { name = "[P]eek", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			["<leader>t"] = { name = "nvim-[T]ree/[T]erminal", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
 		})
 	end,
 }
