317e088c8038fc0b721371e86031235f3626b09e
Mike Rivnak <rivnakm1@gmail.com>
2024-12-21 02:01:49 -0500

    Fix some barbar hightlights

--- lua/themes/modus.lua
+++ lua/themes/modus.lua
@@ -2,6 +2,21 @@ return {
 	"miikanissi/modus-themes.nvim",
 	priority = 1000,
 	config = function()
+		require("modus-themes").setup({
+			style = "modus_vivendi",
+			variant = "default",
+
+			---@param colors ColorScheme
+			---@diagnostic disable-next-line: unused-local
+			on_colors = function(colors) end,
+
+			---@param highlights Highlights
+			---@param colors ColorScheme
+			on_highlights = function(highlights, colors)
+				highlights.BufferVisible = { bg = colors.bg_inactive }
+				highlights.BufferVisibleSign = { bg = colors.bg_inactive }
+			end,
+		})
 		vim.cmd.colorscheme("modus_vivendi")
 	end,
 }
