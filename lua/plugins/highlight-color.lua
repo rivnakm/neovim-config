07b0a587f09bab799536f0aa96254419f299e54d
Mike Rivnak <rivnakm1@gmail.com>
2026-09-08 01:31:22 -0400

    update syntax

--- lua/plugins/highlight-color.lua
+++ lua/plugins/highlight-color.lua
@@ -1,6 +1,6 @@
 -- Highlight colors in code
 return {
-	url = "https://github.com/brenoprata10/nvim-highlight-colors",
+	"brenoprata10/nvim-highlight-colors",
 	opts = {
 		enable_tailwind = true,
 		enable_named_colors = false,
